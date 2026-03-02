c24028c3c305337c185040c854afde1d06adfb49
Alelop-Itu <Alejandralopez_itu@hotmail.com>
2026-03-01 22:28:25 -0500

    Working

--- src/main/java/com/bank/app/infrastructure/controller/MovementController.java
+++ src/main/java/com/bank/app/infrastructure/controller/MovementController.java
@@ -1,4 +1,34 @@
 package com.bank.app.infrastructure.controller;
 
+
+import com.bank.app.application.dto.MovementDTO;
+import com.bank.app.domain.model.Movement;
+import com.bank.app.domain.service.MovementService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/movements")
+@RequiredArgsConstructor
+@Slf4j
+
 public class MovementController {
+    private final MovementService movementService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<MovementDTO> create(@RequestBody MovementDTO dto) {
+        log.info("Petición de movimiento recibida para la cuenta: {}", dto.getAccountNumber());
+        return movementService.createMovement(dto);
+    }
+
+    @GetMapping
+    public Flux<MovementDTO> getAll() {
+        log.info("Consultando los movimientos");
+        return movementService.getAllMovements();
+    }
 }
